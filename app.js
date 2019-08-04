93684378bb879203fb1d6a88347c9c5ebdf83718
Patryk Szafran <bragon912@gmail.com>
2019-08-04 21:27:06 +0200

    auth router

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const appConfig = require('./config');
 
+const authRouter = require('./routes/auth');
+
 appConfig();
 const app = express();
+
+app.use('/auth', authRouter);
 
 app.listen(3000, () => {
     console.log('Listen on the port 3000');
