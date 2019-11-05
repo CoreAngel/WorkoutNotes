b65f9b9a9c7efda8658eb6fc3c718ec616e52b97
Patryk Szafran <bragon912@gmail.com>
2019-11-05 12:25:08 +0100

    new color palette

--- client/src/utils/colors.js
+++
@@ -1,8 +0,0 @@
-export default {
-    white: '#FFFFFF',
-    white70: 'rgba(255, 255, 255, 0.7)',
-    primary: '#FD9F9F',
-    grey: '#707070',
-    secondary: '#212121',
-    dark: '#303030',
-};
---
+++ client/src/utils/colors.tsx
@@ -0,0 +1,9 @@
+
+export enum COLORS {
+    WHITE = '#FFFFFF',
+    WHITE70 = 'rgba(255, 255, 255, 0.7)',
+    PRIMARY = '#FD9F9F',
+    GRAY = '#707070',
+    SECONDARY = '#212121',
+    DARK = '#303030',
+}
