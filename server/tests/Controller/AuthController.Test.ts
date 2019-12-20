76fa21c9afe7711284771f9461d131f8ae923759
Patryk Szafran <bragon912@gmail.com>
2019-12-20 20:44:25 +0100

    Server: Login route tests

--- server/tests/Controller/AuthController.Test.ts
+++ server/tests/Controller/AuthController.Test.ts
@@ -5,6 +5,10 @@ import { AuthController } from '../../src/Controllers/AuthController';
 import { UserService } from '../../src/Services/UserService';
 import { Register } from '../../src/Validators/RegisterValidator';
 import { ValidationErrorException } from '../../src/Exceptions/ErrorResults/ValidationErrorException';
+import { Login } from '../../src/Validators/LoginValidator';
+import { UserModel } from '../../src/Models/UserModel';
+import { Crypto } from '../../src/Utils/Crypto';
+import { LoginErrorException } from '../../src/Exceptions/ErrorResults/LoginErrorException';
 
 describe('Auth Controller', () => {
     describe('Register route', () => {
@@ -61,6 +65,139 @@ describe('Auth Controller', () => {
                 error = e;
             }
             expect(error).to.be.instanceOf(ValidationErrorException);
+            expect((error as ValidationErrorException).getResponse()).to.not.be
+                .null;
+        });
+    });
+
+    describe('Login route', () => {
+        let sandbox: SinonSandbox;
+
+        before(() => {
+            sandbox = createSandbox();
+        });
+        afterEach(() => {
+            sandbox.restore();
+        });
+
+        it('It should login user', async () => {
+            const body: Login = {
+                login: 'user',
+                password: '12341234'
+            };
+            const createUser = sandbox.stub(UserService, 'getUserByLogin');
+            createUser.resolves(
+                new UserModel({
+                    login: body.login,
+                    password: body.password,
+                    email: 'email@gmail.com',
+                    key: 'secretkey'
+                })
+            );
+
+            const comparePasswords = sandbox.stub(Crypto, 'comparePassword');
+            comparePasswords.resolves(true);
+
+            const req = mock.createRequest({ body });
+            const res = mock.createResponse();
+
+            const authController = new AuthController();
+            await authController.login(req, res);
+            const json = res._getData();
+
+            expect(res.statusCode).to.equal(200);
+            expect(json).to.haveOwnProperty('token');
+            expect(json).to.haveOwnProperty('token').not.empty;
+        });
+
+        it('It should throw validation error', async () => {
+            const body: Login = {
+                login: 'u',
+                password: '12341234'
+            };
+            const createUser = sandbox.stub(UserService, 'getUserByLogin');
+            createUser.resolves(
+                new UserModel({
+                    login: body.login,
+                    password: body.password,
+                    email: 'email@gmail.com',
+                    key: 'secretkey'
+                })
+            );
+
+            const comparePasswords = sandbox.stub(Crypto, 'comparePassword');
+            comparePasswords.resolves(true);
+
+            const req = mock.createRequest({ body });
+            const res = mock.createResponse();
+
+            const authController = new AuthController();
+            let error: object | null = null;
+            try {
+                await authController.login(req, res);
+            } catch (e) {
+                error = e;
+            }
+            expect(error).to.be.instanceOf(ValidationErrorException);
+            expect((error as ValidationErrorException).getResponse()).to.not.be
+                .null;
+        });
+
+        it('It should throw login error by wrong login', async () => {
+            const body: Login = {
+                login: 'user',
+                password: '12341234'
+            };
+            const createUser = sandbox.stub(UserService, 'getUserByLogin');
+            createUser.resolves(null);
+
+            const comparePasswords = sandbox.stub(Crypto, 'comparePassword');
+            comparePasswords.resolves(true);
+
+            const req = mock.createRequest({ body });
+            const res = mock.createResponse();
+
+            const authController = new AuthController();
+            let error: object | null = null;
+            try {
+                await authController.login(req, res);
+            } catch (e) {
+                error = e;
+            }
+            expect(error).to.be.instanceOf(LoginErrorException);
+            expect((error as LoginErrorException).getResponse()).to.not.be.null;
+        });
+
+        it('It should throw login error by wrong password', async () => {
+            const body: Login = {
+                login: 'user',
+                password: '12341234'
+            };
+            const createUser = sandbox.stub(UserService, 'getUserByLogin');
+            createUser.resolves(
+                new UserModel({
+                    login: body.login,
+                    password: body.password,
+                    email: 'email@gmail.com',
+                    key: 'secretkey'
+                })
+            );
+
+            const comparePasswords = sandbox.stub(Crypto, 'comparePassword');
+            comparePasswords.resolves(false);
+
+            const req = mock.createRequest({ body });
+            const res = mock.createResponse();
+
+            const authController = new AuthController();
+            let error: object | null = null;
+            try {
+                await authController.login(req, res);
+            } catch (e) {
+                error = e;
+            }
+            expect(error).to.be.instanceOf(LoginErrorException);
+            expect((error as LoginErrorException).getResponse()).to.not.be.null;
         });
     });
 });
