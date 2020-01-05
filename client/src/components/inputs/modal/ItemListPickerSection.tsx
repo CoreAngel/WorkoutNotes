7adacf6a2daab25e175135946f33bedf882a24d5
CoreAngel <bragon912@gmail.com>
2020-01-05 22:54:08 +0100

    Client: Styling section title in sectionlist

--- client/src/components/inputs/modal/ItemListPickerSection.tsx
+++ client/src/components/inputs/modal/ItemListPickerSection.tsx
@@ -52,7 +52,7 @@ const ItemListSelect: FC<Props> = ({ items, setVisible }: Props) => {
     }) => React.ReactElement | null;
     const renderSection: RenderSectionType = ({ section }: paramsSection) => {
         const sectionPicker = section as PickerSectionItem;
-        return <ItemText>{sectionPicker.title}</ItemText>;
+        return <SectionText>{sectionPicker.title}</SectionText>;
     };
 
     const SectionListPicker = SectionList as SectionList<PickerItem>;
@@ -80,6 +80,15 @@ const ItemText = styled(DefaultTextFont)`
     color: ${Colors.WHITE};
     font-size: 18px;
     padding: 10px 10px;
+`;
+
+const SectionText = styled(DefaultTextFont)`
+    color: ${Colors.WHITE70};
+    font-size: 16px;
+    padding: 5px 5px;
+    margin: 5px 5px;
+    border-bottom-color: ${Colors.PRIMARY};
+    border-bottom-width: 1px;
 `;
 
 const NoItemsInfo = styled(DefaultTextFont)`
