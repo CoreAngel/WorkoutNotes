63f12ed06316ba72645cc1af9e44e9dc0717d391
CoreAngel <bragon912@gmail.com>
2020-01-19 19:51:47 +0100

    Client: Picker add optional label

--- client/src/components/inputs/Picker/Picker.tsx
+++ client/src/components/inputs/Picker/Picker.tsx
@@ -1,15 +1,19 @@
 import React, { FC, useState } from 'react';
 import { View } from 'react-native';
+import styled from 'styled-components/native';
 import { AddSmallIcon } from '../../icons';
 import Modal from '../Modal';
 import ItemListPicker, { PickerItem } from './ItemListPicker';
+import { DefaultText } from '../../DefaultText';
+import Colors from '../../../utils/Colors';
 
 type Props = {
+    label?: string;
     items: PickerItem[];
     onChange: (item: PickerItem) => void;
 };
 
-const Picker: FC<Props> = ({ items, onChange }: Props) => {
+const Picker: FC<Props> = ({ items, onChange, label }: Props) => {
     const [isModalVisible, setModalVisible] = useState(false);
 
     const selectItem = (item: PickerItem) => {
@@ -20,12 +24,25 @@ const Picker: FC<Props> = ({ items, onChange }: Props) => {
 
     return (
         <View>
-            <AddSmallIcon onClick={() => setModalVisible(true)} />
+            <Container>
+                <AddSmallIcon onClick={() => setModalVisible(true)} />
+                {label && <Label>{label}</Label>}
+            </Container>
             <Modal isVisible={isModalVisible} setIsVisible={setModalVisible}>
                 <ItemListPicker items={items} onChange={selectItem} />
             </Modal>
         </View>
     );
 };
+
+const Container = styled.View`
+    flex-direction: row;
+    align-items: center;
+`;
+
+const Label = styled(DefaultText)`
+    color: ${Colors.PRIMARY};
+    margin-left: 5px;
+`;
 
 export default Picker;
